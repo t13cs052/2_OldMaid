c7c5479bdc532823874cba1714263172f1351032
t13cs052 <t13cs052@yamanashi.ac.jp>
2015-11-18 15:52:00 +0900

    捨てたカードを墓地リストに追加

--- src/Table.java
+++ src/Table.java
@@ -1,22 +1,24 @@
 /**
- * テーブルを表すクラス。
- */
-
+* テーブルを表すクラス。
+*/
 import java.util.ArrayList;
-
 public class Table {
-
 	/** プロパティ */
 	/** 捨てられたカードを保持しておくためのリスト */
-    private ArrayList disposedCards_ = new ArrayList();
-
-    /** メソッド */
-    /**
-     * カードを捨てる。
-     * 
-     * @param card 捨てるカードの配列
-     */
-    public void disposeCard(Card card[])
-    {
-    }
+	private ArrayList disposedCards_ = new ArrayList();
+	/** メソッド */
+	/**
+	 * カードを捨てる。
+	 *
+	 * @param card 捨てるカードの配列
+	 */
+	public void disposeCard(Card card[]){
+		for (int index=0; index < card.length; index++ ){
+			// 捨てられたカードを表示する
+				System.out.print(card[index] + " ");
+		}
+		System.out.println("を捨てました");
+		// 捨てられたカードは Table クラスの disposedCards_ に追加
+		disposedCards_.add(card);
+	}
 }
